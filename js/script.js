499eafbb86797fe1a75961e670a81bfa04b21439
Aidan OReilly <ajoreilly93@gmail.com>
2021-03-09 09:27:06 -0700

    updatedscript to optimize fetch. css tuning added

--- js/script.js
+++ js/script.js
@@ -1,100 +1,62 @@
-/*Semi Functional!
-  Some of the cards in the obj array from this API dont actually contain an imageUrl, so when cardUrl comes back as undefined, an error occurs and a card is not produced on the page. This obviously causes some functionality issues. sometimes requiring you to click 'Draw' multiple times to get a card, and cause 'Draw Hand' to not consistently produce a full seven card hand. */
-
-//html variables
-const drawBtn = document.querySelector('.draw');
-const handBtn = document.querySelector('.hand');
-const discBtn = document.querySelector('.discard')
-const container = document.querySelector('main')
-const p = document.querySelector('test')
+const insertCard = (card) => {
+  const container = document.querySelector('main')
+  //html elements created on click
+  const section = document.createElement('section')
+  container.appendChild(section);
+  const h2 = document.createElement('h2');
+  section.appendChild(h2);
+  const img = document.createElement('img');
+  section.appendChild(img);  
+  
+  //name and image URL from cards array put onto page
+  h2.innerText = card.name;
+  // s added to http to fix broken img links
+  img.src = card.imageUrl.slice(0, 4) + 's' + card.imageUrl.slice(4);
+};
 
 //random page variable:
 // randPage = Math.floor(Math.random() * 567)
 
-//random single card generated
-const createCard = () => {
-  const endPoint = `https://api.magicthegathering.io/v1/cards?pageSize=7&random=true&contains=imageUrl`
-fetch(endPoint)
-  .then(res => {
-    if(res.ok) {
-      return res.json();
-    }
-})
-  .then(cards => {
-    //random card variable
-    const cardArr = cards.cards;
-    // console.log(cards);
-    const rand = Math.floor(Math.random() * cardArr.length);
-  
-    //html elements created on click
-    const section = document.createElement('section')
-    container.appendChild(section);
-    const h1 = document.createElement('h1');
-    section.appendChild(h1);
-    const img = document.createElement('img');
-    section.appendChild(img);  
-    
-    //card name variable and card image URL variable
-    const cardName = cardArr[rand].name;
-    const cardUrl = cardArr[rand].imageUrl;
-  
-    // slice used to amend image URL to include https instead http. thx to **Vitaly** for the assist
-    const newCardUrl = cardUrl.slice(0, 4) + 's' + cardUrl.slice(4)
-    
-    //name and image URL from cards array put onto page
-    h1.innerHTML = cardName;
-    img.src = newCardUrl;
-    
-    
-})
+//random card(s) generated
+const createCards = (quantity) => {
+  const apiURL = `https://api.magicthegathering.io/v1/cards?pageSize=${quantity}&random=true`;
+  fetch (apiURL)
+  .then(response => {
+    if(!response.ok){
+      throw new Error('No network response from the API');
+    } else {
+      return response.json();
+    }    
+  })
+  .then((responseJson) => {    
+    const cardsArr = responseJson.cards;
+    cardsArr.forEach(card => {
+      if(!card.imageUrl){
+        createCards(1);
+      }else{
+        insertCard(card);
+      }
+    });
+  })
   .catch(error => {
     console.log(error)
-})
+  });
+};
 
-}
-// card displayed on click
-drawBtn.addEventListener('click', createCard);
+//card displayed on click
+const drawBtn = document.querySelector('.draw');
+drawBtn.addEventListener('click', function() {
+  createCards(1)
+});
 
-//seven card hand is generated
-const createHand = () => {
-  const endPoint = `https://api.magicthegathering.io/v1/cards?pageSize=7&random=true&contains=imageUrl`
-fetch(endPoint)
-  .then(res => {
-    if(res.ok) {
-      return res.json();
-    }
-})
-  .then(cards => {
-    //same code as above, but put into a for loop that produces seven instances of  a random card on click, rather than one
-    for (i = 1; i < 8; i++) {
-      const cardArr = cards.cards;
-      const rand = Math.floor(Math.random() * cardArr.length);
-  
-    
-      const section = document.createElement('section')
-      container.appendChild(section);
-      const h1 = document.createElement('h1');
-      section.appendChild(h1);
-      const img = document.createElement('img');
-      section.appendChild(img);  
-      
-      const cardName = cardArr[rand].name;
-      const cardUrl = cardArr[rand].imageUrl;
-      const newCardUrl = cardUrl.slice(0, 4) + 's' + cardUrl.slice(4)
-    
-      h1.innerHTML = cardName;
-      img.src = newCardUrl;
-      
-    } 
-})
-  .catch(error => {
-    console.log(error)
-}) 
-}
 //hand displayed on click
-handBtn.addEventListener('click', createHand)
+const handBtn = document.querySelector('.hand');
+handBtn.addEventListener('click', function() {
+  createCards(7)
+});
 
 //reload page 'discarding' the card
+const discBtn = document.querySelector('.discard')
 discBtn.addEventListener('click',  function(){
   history.go(0);
-})
+});
